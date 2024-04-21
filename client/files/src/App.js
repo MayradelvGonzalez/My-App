f6430820e3bf232fcfcc0c45229d516182f2167c
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-04-21 16:53:31 -0300

    agregElementos

--- client/files/src/App.js
+++ client/files/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import './index.css';
 function App(){
   const [backendData, setBackendData] = useState([{}])
   useEffect(() => {
@@ -13,7 +13,24 @@ function App(){
   }, [])
   return (
     <div>
-      <h1>Hello World!</h1>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+<div>
+  <div className='grid-container' id='caja'>
+  <header className='header'>HEADER</header>
+  <nav className='navbar'>NAVBAR</nav>
+  <aside className='sidebar'>SIDEBAR</aside>
+  <article className='main'>MAIN</article>
+  <footer className='footer'>FOOTER</footer>
+  </div>
+</div>
+
+
     </div>
   );
 }
