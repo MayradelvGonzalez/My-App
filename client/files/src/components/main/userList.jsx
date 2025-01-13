a3fbd542885051969e33cf6b681ccde28cca6c66
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2025-01-13 13:33:33 -0300

    analizandoInfo

--- client/files/src/components/main/userList.jsx
+++ client/files/src/components/main/userList.jsx
@@ -1,76 +1,25 @@
-// import React, { useState, useEffect } from 'react';
-// import './main.css';
-// function UserList() {
- 
-
-//   const [users, setUsers] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true); // Use setIsLoading
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-   
-//     const fetchData = async () => {
-    
-
-//       setIsLoading(true);
-    
-//       try {
-
-       
-//         const response = await fetch('../data/userdata.json'); // Replace with the correct path
-//         const data = await response.json();
-//         setUsers(data);
-//       } catch (error) {
-//         setError(error.message); // Use error.message for the error message
-//         console.error('Error al obtener los datos:', error);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div id='listado'>
-//       <h1>Lista de Usuarios</h1>
-//       {isLoading ? (
-//         <p>Cargando...</p>
-//       ) : error ? (
-//         <p>Error al cargar: {error}</p>
-//       ) : (
-//         <ul>
-//           {users.map((user) => (
-//             <li key={user.id}>{user.name}</li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default UserList;
-
 import React, { useState, useEffect } from 'react';
 import './main.css';
+import path from 'path';
 
 function UserList() {
   const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // Use setIsLoading
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
 
       try {
-       
-        const response = await fetch('../userdata.json'); // 
+        const filePath = path.resolve(__dirname, '../../server/userdata.json');
+        const response = await fetch(filePath);
         const data = await response.json();
         console.log('Fetched data:', data);
         setUsers(data);
       } catch (error) {
         setError(error.message);
-        console.error('Error al obtener los datos:', error);
+        console.error('Error fetching data:', error);
       } finally {
         setIsLoading(false);
       }
@@ -83,9 +32,9 @@ function UserList() {
     <div id='listado'>
       <h1>Lista de Usuarios</h1>
       {isLoading ? (
-        <p>Cargando...</p>
+        <p>Cargando datos de usuarios...</p>
       ) : error ? (
-        <p>Error al cargar: {error}</p>
+        <p>Error al cargar los usuarios: {error}</p>
       ) : users.length > 0 ? (
         <ul>
           {users.map((user) => (
