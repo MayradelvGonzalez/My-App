e2d8cad9c5fc329a3fdfdc320ee604f34a722051
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2025-01-25 20:29:39 -0300

    arregCarrousele

--- client/src/components/carrousel/Carrousel.jsx
+++ client/src/components/carrousel/Carrousel.jsx
@@ -7,62 +7,18 @@ import imagen1 from '../carrousel/imagen1.avif';
 import imagen2 from '../carrousel/imagen2.avif';
 import './Carrousel.css';
 
-// function Carrousel() {
-//     return (
-//        <Carousel>
-//        <Carousel.Item>
-//        <img style={{height: "500px", border: "1px black solid"}}
-//         className="d-block w-100"
-//        src={MENTA}
-//        alt="Primera imagen"
-      
-//      />
-//          <Carousel.Caption>
-//            <h3>First slide label</h3>
-//            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-//          </Carousel.Caption>
-//        </Carousel.Item>
-//         <Carousel.Item>
-//        <img
-//          className="d-block w-100"
-//        src={MENTA2}
-//        alt="segund imagen"
-     
-//      />
-//          <Carousel.Caption>
-//            <h3>Second slide label</h3>
-//            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//          </Carousel.Caption>
-//        </Carousel.Item>
-//        <Carousel.Item>
-//        <img
-//        className="d-block w-100"
-//        src={MENTA3}
-//        alt="Tercera imagen"
-       
-//      />
-//          <Carousel.Caption>
-//            <h3>Third slide label</h3>
-//            <p>
-//              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-//            </p>
-//          </Carousel.Caption>
-//        </Carousel.Item> 
-//      </Carousel>
-//       );
-// }
 
-// export default Carrousel;
 
 function Carrousel() {
   return (
     <div className="carrousel">
     <Carousel>
     <Carousel.Item>
-      <img style={{height:'60vh', objectFit:'cover'}}
-        className="d-block w-100"
-        src={imagen}
-        alt="First slide"
+      <img 
+      style={{ objectFit: 'contain'}} 
+      className="d-block w-100" 
+      src={imagen} 
+      alt="First slide" 
       />
       <Carousel.Caption>
         <h3>First slide label</h3>
@@ -70,10 +26,10 @@ function Carrousel() {
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img style={{height:'60vh', objectFit:'cover'}}
-        className="d-block w-100"
-        src={imagen2}
-        alt="Second slide"
+      <img style={{ objectFit: 'contain'}} 
+    className="d-block w-100" 
+    src={imagen} 
+    alt="Second slide" 
       />
 
       <Carousel.Caption>
@@ -82,10 +38,11 @@ function Carrousel() {
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img style={{height:'60vh',objectFit:'cover'}}
-        className="d-block w-100"
-        src={imagen1}
-        alt="Third slide"
+      <img 
+     style={{ objectFit: 'contain' }} 
+     className="d-block w-100" 
+     src={imagen2} 
+     alt="First slide" 
       />
 
       <Carousel.Caption>
