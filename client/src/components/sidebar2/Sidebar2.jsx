350ebeeac1e5013857e4eb8c9bda018c551ea7eb
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2025-01-25 23:26:56 -0300

    infoAgre

--- client/src/components/sidebar2/Sidebar2.jsx
+++ client/src/components/sidebar2/Sidebar2.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-
+import './sidebar.css';
 function Sidebar2(){
 return (
 
 <div className="Sidebar2">
 
     <h2>Sección</h2>
+    <div className="contenido">
     <p>tareas opcionales, info extra</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id qui, illo blanditiis velit saepe mollitia similique atque, et, iusto labore quia accusamus deserunt ducimus.</p>
    
-    </div>
+    </div> </div>
 )}
 
 
