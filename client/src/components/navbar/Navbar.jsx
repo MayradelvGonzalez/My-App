5f89b297eb820d7f20b9f1d3cb6f14b0392b9784
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2025-01-30 21:30:19 -0300

    agregLogica

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,22 +1,51 @@
+// import React from "react";
+// import './navbar.css';
+// import { useGrade } from '../../context/GradeContext';
+
+
+// function Navbar(){
+//     const { grade } = useGrade();
+//     return( 
+// <div className="Navbar">
+// {grade === '5' ? (
+//           <p>Info para quinto</p>
+//         ) : (
+//           <p>Info para sexto</p>
+//         )}
+//         <p>Otra sección</p>
+//     </div>
+
+//     )
+    
+// }
+
+// export default Navbar;
 import React from "react";
 import './navbar.css';
 import { useGrade } from '../../context/GradeContext';
 
-
 function Navbar(){
     const { grade } = useGrade();
-    return( 
-<div className="Navbar">
-{grade === '5' ? (
-          <p>Info para quinto</p>
-        ) : (
-          <p>Info para sexto</p>
-        )}
-        <p>Otra sección</p>
-    </div>
 
-    )
-    
+    const getInfoForGrade = (grade) => {
+        switch (grade) {
+            case '5':
+                return <p>Info para quinto</p>;
+            case '6':
+                return <p>Info para sexto</p>;
+            case 'home':
+                return <p>Bienvenido a Home</p>;  // Información para la página Home
+            default:
+                return <p>Selecciona un curso</p>; // Mensaje por defecto
+        }
+    };
+
+    return( 
+        <div className="Navbar">
+            {getInfoForGrade(grade)} {/* Llama a la función para obtener la info */}
+            <p>Otra sección</p> 
+        </div>
+    );
 }
 
 export default Navbar;
