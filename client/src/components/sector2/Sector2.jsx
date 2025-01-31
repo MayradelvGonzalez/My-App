a8a588ac5629b76e4e5ee18eeac2f703d3bfd9c2
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2025-01-31 20:30:49 -0300

    media queries

--- client/src/components/sector2/Sector2.jsx
+++ client/src/components/sector2/Sector2.jsx
@@ -5,7 +5,6 @@ import UserList2 from '../main/userList2.jsx';
 function Sector2({ users }) {
   return (
     <div className="main-container">
-      {/* Otros componentes como Header, Navbar, etc. */}
       <UserList2 users={users} />
     </div>
   );
