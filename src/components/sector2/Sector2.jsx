7768dc6eae004a1ff12a248baf9b5b2ea88c1052
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2025-02-09 22:57:53 -0300

    cajas

--- src/components/sector2/Sector2.jsx
+++ src/components/sector2/Sector2.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-// import './main.css';
+import '../../index.css';
 import UserList2 from '../main/userList2.jsx';
 
 function Sector2({ users }) {
   return (
     <div className="main-container">
+      <div className="alumnos">
       <UserList2 users={users} />
+      </div>
+     
     </div>
   );
 }
