13b09dbf29be128fb0cbceffe198be1d41df7352
Mayra del Valle González <70923438+MayradelvGonzalez@users.noreply.github.com>
2025-02-18 14:23:34 -0300

    correcciones

--- src/components/sector2/Sector2.jsx
+++ src/components/sector2/Sector2.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import '../../index.css';
+// import '../../index.css';
+import './sector2.css';
 import UserList2 from '../main/userList2.jsx';
 
 function Sector2({ users }) {
   return (
+  <div className="alumnado">
     <div className="main-container">
       <div className="alumnos">
       <UserList2 users={users} />
       </div>
      
-    </div>
+    </div></div>
   );
 }
 
