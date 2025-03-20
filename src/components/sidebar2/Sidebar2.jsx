21f0011a05e99eca533bdeef011814a093d13e45
Mayra del Valle González <70923438+MayradelvGonzalez@users.noreply.github.com>
2025-03-20 18:46:48 -0300

    agregInfo

--- src/components/sidebar2/Sidebar2.jsx
+++ src/components/sidebar2/Sidebar2.jsx
@@ -10,9 +10,12 @@ return (
     <div className="contenido">
     <h1><u>Tareas y juegos opcionales</u></h1>
     <ul className="listadoInfo">
+        <><h5>5° grado:</h5>
         <li>Las notas musicales <a href="https://www.musicca.com/es/ejercicios/circulo/1" target="_blank" rel="noreferrer" >Ir a la actividad 🎹</a></li>
+        <h5>6° grado:</h5>
+      
         <li>Práctica de ritmos : <a target="_blank" rel="noreferrer" href="https://aprendomusica.com/const2/04dictadoRitmico/game.html">Ir a la actividad 🪇</a></li>
-        
+        </>
     </ul>
    
    
